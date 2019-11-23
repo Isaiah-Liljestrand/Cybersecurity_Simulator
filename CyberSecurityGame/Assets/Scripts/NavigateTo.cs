c61833993684c939e2af4a80ea0ba00bbc1a47f3
Jacob Marks <jacob.marks@student.nmt.edu>
2019-11-23 11:56:06 -0700

    Created navigation.
    Characters now animate and navigate to set locations. Office changed to have a baked navigation map.

--- CyberSecurityGame/Assets/Scripts/NavigateTo.cs
+++ CyberSecurityGame/Assets/Scripts/NavigateTo.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavigateTo : MonoBehaviour
 {
+    public float GetWithinDistance;
+    public GameObject Test;
+    private Vector3 Target;
+    private bool Walking;
+    private Animator Anm;
+    private NavMeshAgent Agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Walking = false;
+        Anm = GetComponent<Animator>();
+        Agent = GetComponent<NavMeshAgent>();
+        if (Test)
+            GoToObject(Test);
+    }
+
+    public void GoToObject(GameObject obj)
+    {
+        GoToPosition(obj.transform.position);
+    }
+
+    public void GoToPosition(Vector3 pos)
+    {
+        Target = pos;
+        Walking = true;
+        Anm.SetBool("Walking", true);
+        Agent.SetDestination(Target);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Walking)
+        {
+            if (Vector3.Distance(transform.position, Target) < GetWithinDistance)
+            {
+                Walking = false;
+                Anm.SetBool("Walking", false);
+            }
+        }
     }
 }
