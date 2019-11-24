d92644e42ab7cafb4499318ccf5d78c59c13782e
alden Alden <alden.towler@student.nmt.edu>
2019-11-24 01:04:08 -0700

    Update NetworkControl.cs

--- CyberSecurityGame/Assets/Scripts/NetworkControl.cs
+++ CyberSecurityGame/Assets/Scripts/NetworkControl.cs
@@ -81,14 +81,13 @@ public class NetworkControl : MonoBehaviour
 
     public void set_infection_status(GameObject node, int status_code) {
 
-        if (status_code < 0) {
+        if (status_code < 1) {
 
-
-
-
+            status_code = 1;
 
         }else if(status_code > 5) {
 
+            status_code = 5;
 
         }else if (status_code == 1) { // cure it
             node.transform.Find("NetworkNode");
