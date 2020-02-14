5c32a97492d008320aec4176fc439107f7061abc
Jacob Marks <jacob.marks@student.nmt.edu>
2020-02-13 18:38:11 -0700

    Added a little speechbubble with random glyphs.
    The glyphs are from a royalty free font. We can use this for abstract employee communication and maybe talking to the player / asking for help.

---
+++ CyberSecurityGame/Assets/Scripts/RandomGlyph.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RandomGlyph : MonoBehaviour
+{
+    private char minchar = '\ue6aa';
+    private char maxchar = '\ue67f';
+    private TextMeshPro text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        RandomChar();
+    }
+
+    public void RandomChar()
+    {
+        Debug.Log((minchar + Random.Range(0, maxchar - minchar)));
+        text.text = "" + (char)(minchar + Random.Range(0, maxchar - minchar));
+    }
+}
