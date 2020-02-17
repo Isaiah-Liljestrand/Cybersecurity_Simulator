4ed561e4a825ecc0a4eef1eea7d1256aa418e270
Jacob Marks <jacob.marks@student.nmt.edu>
2020-02-16 22:38:43 -0700

    Added pan and zoom to the camera. Gave the player major zoomies.
    Main camera and network camera both zoom. Need to add x,y,z bounds for panning. The player's nav agent now moves faster.

---
+++ CyberSecurityGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public KeyCode pan;
+
+    public float minsize;
+    public float maxsize;
+
+    public float zoomspeed;
+
+    private Vector3 lastpos;
+
+    public List<Camera> zoomcameras;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pan))
+        {
+            lastpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetKey(pan))
+        {
+            //Gets the delta of the worldPos and mousePos
+            Vector3 worldPosDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - lastpos;
+            Debug.Log(worldPosDelta);
+
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x - worldPosDelta.x, Camera.main.transform.position.y - worldPosDelta.y, Camera.main.transform.position.z - worldPosDelta.z);
+
+            //Set previous variables to current state for use in next frame
+            lastpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomspeed;
+        if (Camera.main.orthographicSize < minsize)
+            Camera.main.orthographicSize = minsize;
+        if (Camera.main.orthographicSize > maxsize)
+            Camera.main.orthographicSize = maxsize;
+
+        foreach (Camera cam in zoomcameras)
+            cam.orthographicSize = Camera.main.orthographicSize;
+    }
+}
