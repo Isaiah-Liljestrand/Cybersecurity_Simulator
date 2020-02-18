61705081b70fd825e8c796a20b5bddae3f4f0679
Jacob Marks <jacob.marks@student.nmt.edu>
2020-02-18 11:48:28 -0700

    Started Timelinecontrol script.

---
+++ CyberSecurityGame/Assets/Scripts/TimeLineControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLineControl : MonoBehaviour
+{
+    //public List<Disease> diseases;
+    public TimeLineUIScript timelineui;
+    private int maxcount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxcount = timelineui.maxcount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
