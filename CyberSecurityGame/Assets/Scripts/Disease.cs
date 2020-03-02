199ebbde4afe5f86b5e6e2111d711ac1da860f6c
Isaiah-Liljestrand <46874078+Isaiah-Liljestrand@users.noreply.github.com>
2020-03-01 21:51:01 -0700

    Disease modifications
    Configuring disease spreading

--- CyberSecurityGame/Assets/Scripts/Disease.cs
+++ CyberSecurityGame/Assets/Scripts/Disease.cs
@@ -3,39 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
-
-public enum DiseaseType
-{
-    Dos,
-    Phish
-}
 
 [Serializable]
 public class Disease
 {
     public string diseasename;
     public DiseaseType diseasetype;
-    public int turnsbeforespread;
-    public int currentturn;
+
+    public int spreadtype;      //1 = static, 2 = list, 3 = range
+    
+    public int spreadspeedstatic;
+    
+    public int[] spreadspeedlist;
+    public int spreadspeedlistindex;
+    
+    public int spreadspeedmax;
+    public int spreadspeedmin;
+    
     public Color color;
     public Sprite img;
 
     private List<int> infected_computers;
 
-    public void Turnpassed()
+    public int NextSpread()
     {
-        currentturn++;
-        if (currentturn > turnsbeforespread)
-        {
-            Spread();
-            currentturn = 0;
+        if(spreadtype == 1) {
+            return spreadspeedstatic;
+
+        } else if(spreadtype == 2) {
+            if(spreadspeedindex >= spreadspeedlist.Length) {
+                //Repeats last speed after previously specified length
+                //Potential to change if this if this is to repeat the list or swap to random
+                return spreadspeedlist[spreadspeedlist.Length - 1];
+            }
+            //spreadspeedlistindex should be initialized to 1
+            return spreadspeedstatic[spreadspeedlistindex++];
+
+        } else if(spreadtype == 3) {
+            return UnityEngine.Random.Range(spreadspeedmin, spreadspeedmax + 1);
         }
-    }
-
-    public void Spread()
-    {
-
     }
 
     public void Infect(int computer)
--- CyberSecurityGame/Assets/Scripts/DiseaseControl.cs
+++ CyberSecurityGame/Assets/Scripts/DiseaseControl.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class DiseaseControl : MonoBehaviour
 {
-    public List<Disease> possibledieseases;
+    public List<Disease> possiblediseases;
+    public List<int> nextinfect;
     public List<Disease> diseases = new List<Disease>();
     private TimeLineControl timeline;
+    private NetworkControl network;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         timeline = GetComponent<TimeLineControl>();
+        network = GetComponent<NetworkControl>();
     }
 
-    public void AddDisease(Disease disease)
+    public void PassTurn()
     {
-        diseases.Add(disease);
+        //for(int i = 0; i < nextinfect.Count; i++) {
+        //    nextinfect.IndexOf(i)--;
+        //    if(nextinfect.IndexOf(i) == 0) {
+        //       diseases.IndexOf(i).Infect()
+        //        nextinfect.IndexOf(i) = diseases.IndexOf(i).NextSpread();
+        //    }
+        //}
     }
 
     public void AddDisease(Disease disease, int computer)
     {
         disease.Infect(computer);
         diseases.Add(disease);
+        nextinfect.Add(disease.NextSpread());
     }
 
     public void CleanVirus(Disease disease)
