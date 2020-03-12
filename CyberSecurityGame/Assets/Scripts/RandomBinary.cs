fa1f171f04bab8d710ca221da7ed36db72146fdd
Jacob Marks <jacob.marks@student.nmt.edu>
2020-03-12 14:28:28 -0600

    Made random binary appear on computer screens. If you guys don't like it, don't worry it was easy to add.

---
+++ CyberSecurityGame/Assets/Scripts/RandomBinary.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshPro))]
+public class RandomBinary : MonoBehaviour
+{
+    public int length;
+    public bool includespaces;
+    public int cutoff;
+    private TextMeshPro text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        while (text.text.Length <= length)
+        {
+            if (includespaces)
+                text.text += " 10"[Random.Range(0, 2)];
+            else
+                text.text += "10"[Random.Range(0, 1)];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (includespaces)
+            text.text += " 10"[Random.Range(0, 2)];
+        else
+            text.text += "10"[Random.Range(0, 1)];
+
+        if (text.text.Length > length)
+            text.text = text.text.Substring(cutoff);
+    }
+}
