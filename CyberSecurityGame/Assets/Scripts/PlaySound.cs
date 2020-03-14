230969aefb498796f372b99141be4d21c68d22a0
Jacob Marks <jacob.marks@student.nmt.edu>
2020-03-14 11:52:24 -0600

    Added a random pitch offset for soundeffects. To add variety.

--- CyberSecurityGame/Assets/Scripts/PlaySound.cs
+++ CyberSecurityGame/Assets/Scripts/PlaySound.cs
@@ -6,19 +6,23 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     public List<AudioClip> clips;
+    public float randompitchoffset;
     public bool PlayOnTrigger;
     private AudioSource source;
     private AudioClip originalclip;
+    private float originalpitch;
 
     private void Start()
     {
         source = GetComponent<AudioSource>();
         originalclip = source.clip;
+        originalpitch = source.pitch;
     }
 
     public void Play()
     {
         source.clip = originalclip;
+        SetPitch();
         source.Play();
     }
 
@@ -27,6 +31,7 @@ public class PlaySound : MonoBehaviour
         if (clip < clips.Count)
         {
             source.clip = clips[clip];
+            SetPitch();
             source.Play();
         }
     }
@@ -34,6 +39,7 @@ public class PlaySound : MonoBehaviour
     public void PlayRandom()
     {
         source.clip = clips[Random.Range(0, clips.Count - 1)];
+        SetPitch();
         source.Play();
     }
 
@@ -42,8 +48,14 @@ public class PlaySound : MonoBehaviour
         if (start < clips.Count)
         {
             source.clip = clips[Random.Range(start, clips.Count - 1)];
+            SetPitch();
             source.Play();
         }
+    }
+
+    private void SetPitch()
+    {
+        source.pitch = originalpitch + Random.Range(-randompitchoffset, randompitchoffset);
     }
 
     private void OnTriggerEnter(Collider other)
